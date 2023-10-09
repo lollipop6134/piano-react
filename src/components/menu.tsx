6bdc82ad6811a575cc0c4aba6e21e77b258b6cc3
kirill <lollipop6134@gmail.com>
2023-10-09 09:46:20 +0300

    initial commit

---
+++ src/components/menu.tsx
@@ -0,0 +1,14 @@
+import './menu.css';
+
+export function Menu() {
+    return (
+        <div id="container">
+            <div id="left">
+                <img src="/images/logo.png"></img>
+                <button>Piano</button>
+                <button>Lessons</button>
+            </div>
+            <button>Sign Up</button>
+        </div>
+    )
+}
