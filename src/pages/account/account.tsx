21897b2cefeeb91d9b918d5068eb130b908be2ed
lollipop6134 <lollipop6134@gmail.com>
2023-11-28 11:01:34 +0300

    account changes (css)

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -1,20 +1,18 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../../supabaseClient';
 import { Footer } from '../../components/footer/footer';
+import "./account.css"
 
 interface AccountProps {
   session: any;
 }
 
 export default function Account({ session }: AccountProps) {
-  const [loading, setLoading] = useState<boolean>(true);
   const [username, setUsername] = useState<string | null>(null);
 
   useEffect(() => {
-    let ignore = false;
 
     async function getProfile() {
-      setLoading(true);
       const { user } = session;
 
       const { data, error } = await supabase
@@ -23,32 +21,20 @@ export default function Account({ session }: AccountProps) {
         .eq('id', user.id)
         .single();
 
-      if (!ignore) {
         if (error) {
           console.warn(error);
         } else if (data) {
           setUsername(data.username);
         }
-      }
-
-      setLoading(false);
     }
 
     getProfile();
-
-    return () => {
-      ignore = true;
-    };
   }, [session]);
 
   async function updateProfile(event: React.FormEvent) {
     event.preventDefault();
 
-    setLoading(true);
-    const { user } = session;
-
     const updates = {
-      id: user.id,
       username,
     };
 
@@ -57,38 +43,38 @@ export default function Account({ session }: AccountProps) {
     if (error) {
       alert(error.message);
     }
-
-    setLoading(false);
   }
 
   return (
     <>
       <form onSubmit={(e) => {updateProfile(e)}} >
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
+      <div id='accountInputs'>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            value={session.user.email}
+            disabled
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            required
+            value={username || ''}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          required
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+      <div id='progress'>
+        Your progress: 6/12 (50%)
       </div>
-
-      <div>
-        <button type="submit" disabled={loading}>
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
-      <div>
-        <button type="button" onClick={() => supabase.auth.signOut()}>
-          Sign Out
-        </button>
+      <div id='accountButtons'>
+        <button type="submit">Update</button>
+        <button type="button" onClick={() => supabase.auth.signOut()}>Sign Out</button>
       </div>
     </form>
     <div className='bottom'>
